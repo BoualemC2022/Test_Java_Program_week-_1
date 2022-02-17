a954610f9df66685823559cf7ad652c45c74f071
BoualemC2022 <chihaouiboualem@gmail.com>
2022-02-16 22:59:46 -0500

    IN this I added the Method outside main to print Hello World!

--- FirstProgram.java
+++ FirstProgram.java
@@ -1,7 +1,15 @@
 
-public class  FirstProgram { public void main (string[]args){
+public class  FirstProgram {
+    public static  void main (string[]args){
+        string A="hey warap? "
+                System.out.println (A)
+         HelloW();
+    }
+public static void HelloW(){
+        System.out.println("Hello World!")
+}
 
-}}
+}
 
 
 
